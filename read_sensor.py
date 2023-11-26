584dd7e16a1b3bdafae461a6458f78b9ff2c16ae
Garfander <133745041+Garfander@users.noreply.github.com>
2023-11-26 11:08:29 +0100

    Add files via upload

--- read_sensor.py
+++ read_sensor.py
@@ -3,6 +3,8 @@ import board
 #from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
 import adafruit_lis3mdl as LIS3MDL
 from datetime import datetime
+import os
+import cv2
 
 #sensors setup
 i2c = board.I2C()
@@ -14,19 +16,25 @@ data_file = open('data.txt', 'a')
 now_str = (datetime.now()).strftime("%d/%m/%Y %H:%M:%S")
 data_file.write('\n\n' + now_str + '\n')
 
-scan = True
 
-while scan:
+while True:
     mag_x, mag_y, mag_z = sensor_2.magnetic
     data_string = 'X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f} uT'.format(mag_x, mag_y, mag_z)
     if check_value(mag_x, mag_y, mag_z):
         print("--correct value detected--")
-        #scan = False
+        break
     print(data_string)
     data_file.write(data_string+'\n')
-    time.sleep(0.5)
+    time.sleep(0.2)
 
+#when correct values are detected sleep for 0.2 s and then take picure
+take_picture()
 
 
 def check_value(x_val, y_val, z_val):
-    if x_val*100 in range(2871, 2934) and y_val in range() and  z_val in range():
+    return x_val*100 in range(2871, 2934) and y_val*100 in range(-14218,-14130) and  z_val*100 in range(-4901,-4775)
+
+def take_picture():
+    os.system("[-f ./outputX.jpg] && rm outputX.jpg")
+    os.system("ffmpeg -i /dev/video2 -frames:v 1 outputX.jpg")
+    img = cv2.imread(r'outputX.jpg')
